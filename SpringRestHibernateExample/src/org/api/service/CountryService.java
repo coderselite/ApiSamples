cdbd01d64ded53e8e5a02849e818b56313c07edc
Lakshmi Pisharody <lakshmipisharody@yahoo.com>
2017-04-05 15:57:43 +0530

    SpringHibernateRestExample

---
+++ SpringRestHibernateExample/src/org/api/service/CountryService.java
@@ -0,0 +1,42 @@
+package org.api.service;
+
+import java.util.List;
+
+import org.api.bean.Country;
+import org.api.dao.CountryDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+ 
+@Service("countryService")
+public class CountryService {
+ 
+ @Autowired
+ CountryDAO countryDao;
+ 
+ @Transactional
+ public List getAllCountries() {
+  return countryDao.getAllCountries();
+ }
+ 
+ @Transactional
+ public Country getCountry(int id) {
+  return countryDao.getCountry(id);
+ }
+ 
+ @Transactional
+ public void addCountry(Country country) {
+  countryDao.addCountry(country);
+ }
+ 
+ @Transactional
+ public void updateCountry(Country country) {
+  countryDao.updateCountry(country);
+ 
+ }
+ 
+ @Transactional
+ public void deleteCountry(int id) {
+  countryDao.deleteCountry(id);
+ }
+}
